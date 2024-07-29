e90492414b6db3e5b971998230c1ec97d19b2c1b
itsuki <itsuki.enjyoy@gmail.com>
2024-07-30 08:49:54 +0900

    Initial Commit

---
+++ SwiftExtensionCheatSheet/SwiftExtensionCheatSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExtensionCheatSheetApp.swift
+//  SwiftExtensionCheatSheet
+//
+//  Created by Itsuki on 2024/07/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExtensionCheatSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
